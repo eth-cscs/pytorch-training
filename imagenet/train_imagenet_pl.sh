53a198773c274fe409ee73eeb34d20ed1524b919
Henrique Mendonca <henrique@apache.org>
2021-11-24 09:59:24 +0100

    dist imagenet sbatch

--- imagenet/train_imagenet_pl.sh
+++ imagenet/train_imagenet_pl.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -l
-## Usage:
-## > sbatch -N4 train_imagenet.sh --epochs 4 --gpu-aug <additional arguments>
-
 #SBATCH --job-name=imagenet
 #SBATCH --time=04:00:00
 #SBATCH --nodes=2
@@ -19,18 +16,11 @@ export NCCL_ALGO=Tree
 export NCCL_NTHREADS=512
 export PMI_NO_PREINITIALIZE=1
 
-export MASTER_ADDR=$(hostname)
-export MASTER_PORT=29500
-export WORLD_SIZE=$SLURM_NNODES
-export LOCAL_RANK=0
-
-CHECKPOINT_DIR=$SCRATCH/runs/imagenet/$SLURM_JOBID/
-CMD="\
-  python train_imagenet.py /scratch/snx3000/datasets/imagenet/ILSVRC2012_1k \
-  --save-path $CHECKPOINT_DIR \
-  $@ \
-"
+DIR=$SCRATCH/runs/imagenet/pl$SLURM_JOBID
 
 set -x
-mkdir -p $CHECKPOINT_DIR
-srun -ul bash -c "RANK=\$SLURM_NODEID $CMD"
+mkdir -p $DIR
+srun -ul python train_imagenet_pl.py \
+    /scratch/snx3000/datasets/imagenet/ILSVRC2012_1k \
+    --root-dir $DIR \
+    $@
