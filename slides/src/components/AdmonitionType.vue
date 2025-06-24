210cc0e37d9bdde009e25919317b1a947283c65d
boeschf <48126478+boeschf@users.noreply.github.com>
2025-06-24 14:46:38 +0200

    Add admonitions (#14)

---
+++ slides/src/components/AdmonitionType.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'info',
+  },
+  width: {
+    type: String,
+    default: '100%',
+  },
+})
+
+const admontype = computed(() => {
+  switch (props.type) {
+    case 'important':
+      return {
+        icon: 'mdi-message-alert-outline',
+        title: 'Important',
+        color: 'purple-light',
+      }
+    case 'tip':
+      return {
+        icon: 'mdi-lightbulb-outline',
+        title: 'Tip',
+        color: 'emerald-light',
+      }
+    case 'warning':
+      return {
+        icon: 'mdi-alert-outline',
+        title: 'Warning',
+        color: 'amber-light',
+      }
+    case 'caution':
+      return {
+        icon: 'mdi-alert-octagon-outline',
+        title: 'Caution',
+        color: 'red-light',
+      }
+    case 'info':
+    default:
+      return {
+        icon: 'mdi-information-variant-circle-outline',
+        title: 'Note',
+        color: 'sky-light',
+      }
+  }
+})
+</script>
+
+<template>
+  <Admonition :title="admontype.title" :color="admontype.color" :icon="admontype.icon" :width="width">
+    <slot></slot>
+  </Admonition>
+</template>
